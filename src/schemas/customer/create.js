8c3edbf2582c43f14d37c4275a5a0fc8bfb2bea0
Rona ¯\_(ツ)_/¯ <ronauli.sidabukke@btpn.com>
2021-08-13 13:54:46 +0700

    feat(ms): fix create schema

--- src/schemas/customer/create.js
+++ src/schemas/customer/create.js
@@ -2,12 +2,10 @@ const Joi = require('joi');
 
 const createSchema = {
   body: Joi.object().keys({
-    payment_id: Joi.string().trim().required(),
-    payment_code: Joi.string().trim().required(),
-    paid_at: Joi.string().trim().required(),
-    external_id: Joi.string().trim().required(),
+    name: Joi.string().trim().required(),
+    callback_url: Joi.string().trim().required(),
     customer_id: Joi.string().trim().required(),
-    amount: Joi.number().required(),
+    authentication_key: Joi.string().trim().required(),
   }),
 };
 
